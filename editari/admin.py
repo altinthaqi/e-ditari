00e3a391e4031a0fe0ebd0622c27dc561debb664
Erblin Berisha <berishaerblin@gmail.com>
2020-11-06 10:46:12 +0100

    Changing how records of Profile table are shown on Admin site

--- editari/admin.py
+++ editari/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Newsletter, Profile
 
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'birth_date', 'type')
+    ordering = ('birth_date',)
+
+
 admin.site.register(Newsletter)
-admin.site.register(Profile)
+#admin.site.register(Profile)
