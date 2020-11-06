817b2e6ff15aa80cfc51f594b80915b702f71228
Erblin Berisha <berishaerblin@gmail.com>
2020-11-06 10:46:12 +0100

    Changing how records of Profile table are shown on Admin site

--- editari/models.py
+++ editari/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.html import format_html
 
 class Newsletter(models.Model):
     email = models.EmailField(max_length=254, unique=True)
@@ -23,6 +24,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth_date = models.DateField(null=True, blank=True)
     type = models.CharField(max_length=10)
+    list_display = (user, birth_date, type)
 
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
